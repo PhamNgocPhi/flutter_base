69eeaf3fea39dda6d30346e20a73d6b003b84c20
Pham Ngoc Phi <phamngocphi@MacBook-Pro-cua-Pham.local>
2022-07-29 17:45:16 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/base/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.base.flutter_base
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_base/app/app.dart';
+import 'package:flutter_base/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
